1c6c666c816aeedefcad83bb2cd23982a48455f6
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-11 22:42:08 +0200

    Add Results file. Move results of Pet in. Fill in component with the spread operator

---
+++ src/Results.jsx
@@ -0,0 +1,13 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => <Pet {...pet} key={pet.id} />)
+      )}
+    </div>
+  );
+};
