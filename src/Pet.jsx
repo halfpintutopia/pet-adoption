3095f7b899745a07b600aa116666ef1431eab41b
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-12 08:49:08 +0200

    Fix typo in href for each Pet component link

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -5,7 +5,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <a href="{`details/${id}" className="pet">
+    <a href={`details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
